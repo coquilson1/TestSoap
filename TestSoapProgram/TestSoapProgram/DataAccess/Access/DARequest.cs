984180fda49c1680252f56dd05e77aa4a9090a60
coquilson1 <i.infante.dacruz@outlook.com>
2023-07-17 08:05:56 -0500

    Close to the end

--- TestSoapProgram/TestSoapProgram/DataAccess/Access/DARequest.cs
+++ TestSoapProgram/TestSoapProgram/DataAccess/Access/DARequest.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using TestSoapProgram.DataAccess.Models;
 
 namespace TestSoapProgram.DataAccess.Access
